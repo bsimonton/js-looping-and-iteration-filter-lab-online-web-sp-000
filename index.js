611be25e515f03f9a8bda97199b1c5f271ced632
Brian Simonton <bsimonton78@gmail.com>
2019-06-28 20:19:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,12 @@ function fuzzyMatch(drivers, string){
  
  function matchName(drivers, string){
    
-   
+   return drivers.filter(function(driver){return driver.name === string});
    
    
  }
+ 
+ 
+ 
+ 
  
